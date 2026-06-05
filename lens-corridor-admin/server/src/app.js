074e9e6037a7000c63565dd3152712d5788750c0
workholics-shikha <workholics.shikha@gmail.com>
2026-06-05 17:50:40 +0530

    cors fixed for render

--- lens-corridor-admin/server/src/app.js
+++ lens-corridor-admin/server/src/app.js
@@ -10,6 +10,7 @@ const defaultAllowedOrigins = [
   'http://127.0.0.1:3000',
   'http://192.168.29.202:5173',
   'http://127.0.0.1:5173',
+  'https://lenscorridor.onrender.com',
 ];
 
 const configuredOrigins = (
